601f46ac6f962ba993c032e4c2ac38022cb51c94
Saflin Hussain <saflin@giffgaff.co.uk>
2017-10-17 11:19:53 +0100

    update post provisioning steps

--- ansible/post_provision/install_openshift.sh
+++ ansible/post_provision/install_openshift.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 export ANSIBLE_HOST_KEY_CHECKING=False
+
+ansible-playbook -i /bootstrap/ansible/openshift-inventory  /opt/openshift-ansible/PRE_INSTALLATION/giffgaff_openshift_requirements.yml
+
+ansible-playbook -i /bootstrap/ansible/openshift-inventory  /opt/openshift-ansible/playbooks/byo/openshift_facts.yml
+
 ansible-playbook -i /bootstrap/ansible/openshift-inventory  /opt/openshift-ansible/playbooks/byo/config.yml
 
+ansible-playbook -i /bootstrap/ansible/openshift-inventory /opt/openshift-ansible/POST_INSTALLATION/openshift_giffgaff.yml
